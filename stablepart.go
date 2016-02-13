5b63c9d5ca4c48a61ce23336244625aaaf4f23a4
Damian Gryski <dgryski@gmail.com>
2016-02-13 20:08:09 +0100

    docs++

--- stablepart.go
+++ stablepart.go
@@ -2,6 +2,9 @@ package stablepart
 
 import "sort"
 
+// StablePartition partitions the data d[first:last] according to the predicate
+// It returns the index i such that the elements d[:i] satisfy the predicate
+// and d[i:] do not.
 func StablePartition(d sort.Interface, first, last int, pred func(i int) bool) int {
 
 	n := last - first
@@ -22,6 +25,8 @@ func StablePartition(d sort.Interface, first, last int, pred func(i int) bool) i
 	return Rotate(d, StablePartition(d, first, mid, pred), mid, StablePartition(d, mid, last, pred))
 }
 
+// Rotate rotates d[first:last] by k positions so that d[k] is now at d[first].
+// It returns the new index of d[first].
 func Rotate(d sort.Interface, first, k, last int) int {
 	Reverse(d, first, k)
 	Reverse(d, k, last)
@@ -29,6 +34,7 @@ func Rotate(d sort.Interface, first, k, last int) int {
 	return first + last - k
 }
 
+// Reverse reverses d[first:last]
 func Reverse(d sort.Interface, first, last int) {
 	lend := (last - first)
 	for i, j := 0, lend-1; i < lend/2; i, j = i+1, j-1 {
@@ -36,6 +42,9 @@ func Reverse(d sort.Interface, first, last int) {
 	}
 }
 
+// Slide shifts d[first:last] to target position pos and returns the new
+// indices of the sublist.  If pos < first, the list slides so that the sublist
+// is at d[pos:X]; if p > last, the resulting list is at d[X:pos].
 func Slide(d sort.Interface, first, last, pos int) (int, int) {
 	if pos < first {
 		return pos, Rotate(d, pos, first, last)
@@ -46,6 +55,7 @@ func Slide(d sort.Interface, first, last, pos int) (int, int) {
 	return first, last
 }
 
+// Gather groups together elements satisfying pred around the target position pos.
 func Gather(d sort.Interface, first, last, pos int, pred func(i int) bool) (int, int) {
 	notp := func(i int) bool { return !pred(i) }
 	return StablePartition(d, first, pos, notp), StablePartition(d, pos, last, pred)
